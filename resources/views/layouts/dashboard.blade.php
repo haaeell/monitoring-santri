9598bbeecca0f9f1ef9e4c7c2150600e83d053f7
haaeell <haikaljawahir434@gmail.com>
2024-12-02 23:55:23 +0700

    fix: sidebar guru

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -7,16 +7,16 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Monitoring Pondok Pesantren</title>
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/feather/feather.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/datatables.net-bs5/dataTables.bootstrap5.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/js/select.dataTables.min.css">
-    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
-    <link rel="shortcut icon" href="{{asset('assets')}}/images/favicon.png" />
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/feather/feather.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/datatables.net-bs5/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets') }}/js/select.dataTables.min.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/style.css">
+    <link rel="shortcut icon" href="{{ asset('assets') }}/images/favicon.png" />
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.min.css" rel="stylesheet" />
@@ -84,7 +84,7 @@
                         <span class="mx-2">{{ Auth::user()->name }}</span>
                         <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
                             id="profileDropdown">
-                            <img src="{{asset('assets')}}/images/faces/face28.jpg" alt="profile" />
+                            <img src="{{ asset('assets') }}/images/faces/face28.jpg" alt="profile" />
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
                             aria-labelledby="profileDropdown">
@@ -113,8 +113,8 @@
                         </a>
                     </li>
                     <hr>
-                    @if (Auth::user()->role == 'admin')
-                    <span>MASTER DATA</span>
+                    @if (Auth::user()->role == 'admin' || Auth::user()->role == 'kepala_pondok')
+                        <span>MASTER DATA</span>
                         <li class="nav-item">
                             <a class="nav-link" href="/santri">
                                 <i class="icon-paper menu-icon"></i>
@@ -157,25 +157,35 @@
                                 <span class="menu-title">Mapel</span>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/setor">
-                                <i class="icon-paper menu-icon"></i>
-                                <span class="menu-title">Setor Hafalan</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/setor">
-                                <i class="icon-paper menu-icon"></i>
-                                <span class="menu-title">Absensi</span>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/setor">
-                                <i class="icon-paper menu-icon"></i>
-                                <span class="menu-title">Nilai</span>
-                            </a>
-                        </li>
                     @endif
+                    @if (Auth::user()->role == 'guru')
+                            <li class="nav-item">
+                                <a class="nav-link" href="/setor">
+                                    <i class="icon-paper menu-icon"></i>
+                                    <span class="menu-title">Setor Hafalan</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/absensi">
+                                    <i class="icon-paper menu-icon"></i>
+                                    <span class="menu-title">Absensi</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/nilai">
+                                    <i class="icon-paper menu-icon"></i>
+                                    <span class="menu-title">Nilai</span>
+                                </a>
+                            </li>
+                    @endif   
+                    @if (Auth::user()->role == 'wali_santri')
+                    <li class="nav-item">
+                        <a class="nav-link" href="/nilai">
+                            <i class="icon-paper menu-icon"></i>
+                            <span class="menu-title">Nilai Santri</span>
+                        </a>
+                    </li>
+                    @endif   
                     <li class="nav-item">
                         <a class="nav-link" data-bs-toggle="collapse" href="#error" aria-expanded="false"
                             aria-controls="error">
@@ -203,18 +213,18 @@
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="{{asset('assets')}}/vendors/js/vendor.bundle.base.js"></script>
-    <script src="{{asset('assets')}}/vendors/chart.js/chart.umd.js"></script>
-    <script src="{{asset('assets')}}/vendors/dutatatables.net/jquery.dataTables.js"></script>
-    <script src="{{asset('assets')}}/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-    <script src="{{asset('assets')}}/vendors/datatables.net-bs5/dataTables.bootstrap5.js"></script>
-    <script src="{{asset('assets')}}/js/dataTables.select.min.js"></script>
-    <script src="{{asset('assets')}}/js/off-canvas.js"></script>
-    <script src="{{asset('assets')}}/js/template.js"></script>
-    <script src="{{asset('assets')}}/js/settings.js"></script>
-    <script src="{{asset('assets')}}/js/todolist.js"></script>
-    <script src="{{asset('assets')}}/js/jquery.cookie.js" type="text/javascript"></script>
-    <script src="{{asset('assets')}}/js/dashboard.js"></script>
+    <script src="{{ asset('assets') }}/vendors/js/vendor.bundle.base.js"></script>
+    <script src="{{ asset('assets') }}/vendors/chart.js/chart.umd.js"></script>
+    <script src="{{ asset('assets') }}/vendors/dutatatables.net/jquery.dataTables.js"></script>
+    <script src="{{ asset('assets') }}/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+    <script src="{{ asset('assets') }}/vendors/datatables.net-bs5/dataTables.bootstrap5.js"></script>
+    <script src="{{ asset('assets') }}/js/dataTables.select.min.js"></script>
+    <script src="{{ asset('assets') }}/js/off-canvas.js"></script>
+    <script src="{{ asset('assets') }}/js/template.js"></script>
+    <script src="{{ asset('assets') }}/js/settings.js"></script>
+    <script src="{{ asset('assets') }}/js/todolist.js"></script>
+    <script src="{{ asset('assets') }}/js/jquery.cookie.js" type="text/javascript"></script>
+    <script src="{{ asset('assets') }}/js/dashboard.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 
